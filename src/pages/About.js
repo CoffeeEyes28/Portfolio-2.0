21e4cbb82e092a14848c0ca709112ff093ed794b
CoffeeEyes28 <liv@Olivias-MacBook-Air.local>
2022-12-29 21:11:44 -0500

    added image and bio to about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,8 +1,12 @@
 import React from "react";
 
+
+import aboutPic from '../Assets/Images/about.JPG'
 import { motion } from 'framer-motion'
 
+import { Container } from "react-bootstrap";
 
+import Image from 'react-bootstrap/Image'
 
 
 
@@ -23,7 +27,28 @@ export default function About(){
         animate={{y: 0, opacity: 1}}
         exit={{y: 20, opacity: 0}}
         >
+        <Container className="text-white m-2 p-4 d-flex flex-wrap">
+            <div className="d-flex flex-row align-items-center">
             
+            <div className="d-flex flex-col align-items-center">
+            <Container>
+            <Image className="w-100 shadow rounded-circle"  src={aboutPic} alt='Gregory Johnston'/>
+            </Container>
+            </div>
+
+            <div className="d-flex col-6 text-center">
+            <Container>
+            <h1 >About</h1>
+          
+            <p > Greg is a Full Stack Web Developer with a background in customer service and team management. They are considred to be a fast learner and resilient in the face of any challenge. Amongst their peers and team members they have been highly regarded as dependable and a positive influence on morale. </p>
+            
+            
+            </Container>
+            </div>
+
+
+            </div>
+        </Container>
         </motion.div>
     )
 }
