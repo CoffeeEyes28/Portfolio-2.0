53426548751bd016b0946bd132a4654bcf680d3e
Greg <litm47@gmail.com>
2023-02-10 14:33:47 -0500

    maded changes to resume section and added resume download file

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import resume from '../Assets/GregoryJohnstonResume.docx';
 import { motion } from 'framer-motion';
 import Timeline from '@mui/lab/Timeline';
 import TimelineItem from '@mui/lab/TimelineItem';
@@ -7,6 +8,8 @@ import TimelineSeparator from '@mui/lab/TimelineSeparator';
 import TimelineConnector from '@mui/lab/TimelineConnector';
 import TimelineContent from '@mui/lab/TimelineContent';
 import TimelineDot from '@mui/lab/TimelineDot';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
 
 
 
@@ -17,9 +20,21 @@ const Resume = () => {
     animate={{y: 0, opacity: 1}}
     exit={{y: 20, opacity: 0}}>
 
-            <diiv className="d-flex justify-content-center">
+            <div className="d-flex flex-row justify-content-center">
             <h1 className='text-light'>Resume</h1>
-            </diiv>
+            </div>
+            <br></br>
+            <div className='d-flex flex-row justify-content-center'>
+              <div className='d-flex flex-column'>
+              <p className='text-light'>Download a copy of my resume below</p>
+              <br></br>
+            
+              <a className='btn btn-light' download href={resume}>Download</a>
+              
+              
+              </div>
+            
+            </div>
             <br></br>
            <Timeline position='alternate'>
             <TimelineItem>
@@ -27,8 +42,48 @@ const Resume = () => {
                 <TimelineDot />
                 <TimelineConnector/>
               </TimelineSeparator>
-              <TimelineContent></TimelineContent>
+              <TimelineContent>
+                <h2 className='text-light'>Edx Bootcamps (Jan. 2023-Present)</h2>
+                <p className='text-light '>Worked directly with the class instructor to create an engaging and efficient learning environment. 
+                <br></br>
+                Provided technical and material support for over 20 students on various aspects of Full Stack Web Development. 
+                <br></br>
+                Including: HTML, CSS, Javascript, Git, React.js, Node.js, Express.js, SQL, and NoSQL. </p>
+
+              </TimelineContent>
             </TimelineItem>
+            <TimelineItem>
+              <TimelineSeparator>
+                <TimelineDot />
+                <TimelineConnector/>
+              </TimelineSeparator>
+              <TimelineContent>
+                <h3 className='text-light'>Rutgers University  (Dec. 2022)</h3>
+
+              </TimelineContent>
+            </TimelineItem>
+            <TimelineItem>
+              <TimelineSeparator>
+                <TimelineDot />
+                <TimelineConnector/>
+              </TimelineSeparator>
+              <TimelineContent>
+                <h4 className='text-light'>Rite Aid (2018)</h4>
+
+              </TimelineContent>
+            </TimelineItem>
+            <TimelineItem>
+              <TimelineSeparator>
+                <TimelineDot />
+
+              </TimelineSeparator>
+              <TimelineContent>
+                <h5 className='text-light'>Belk Department Store (2016- 2018)</h5>
+
+              </TimelineContent>
+            </TimelineItem>
+
+            
            </Timeline>
 
     </motion.div>
