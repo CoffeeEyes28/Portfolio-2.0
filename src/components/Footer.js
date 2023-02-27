ef16fd97264454c510cd7d0df1ef9954a1394cc0
Greg <litm47@gmail.com>
2023-02-27 12:28:42 -0500

    made changes to footer padding and navbar now hamburgers on flex

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,14 +10,15 @@ import LinkedIn from '../Assets/Images/linkedin.png';
 
 export default function Footer() {
   return (
-        <footer className='fixed-bottom'>
+        <footer className='fixed-bottom py-4' style={{height: '160px'}}>
         <div className='d-flex flex-column justify-content-center bg-dark'>
         <div className='d-flex flex-row justify-content-center text-light'>
           <p style={{fontSize: '18px'}}>Contact Me</p>
         </div>
         <div className='d-flex flex-row justify-content-center text-light'>
-          <p>gjohnston47@yahoo.com</p>
+        <a href='mailto:gjohnton47@yahoo.com'><p>gjohnston47@yahoo.com</p></a> 
         </div>
+       
         <div className='d-flex flex-row justify-content-center align-items-center text-light'>
         <a target="_blank" rel="noreferrer"href="https://github.com/CoffeeEyes28">
         <img alt="GitHub logo"style={{width: '50px', height: '50px'}} className="mx-2"src={GitHub} />
@@ -27,6 +28,7 @@ export default function Footer() {
         <img alt="LinkedIn logo"style={{width: '40px', height: '40px '}} className="mx-2"src={LinkedIn} />
         </a>
         </div>
+        <br></br>
         <br></br>
       
         </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import React from "react";
 import Container  from "react-bootstrap/Container";
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-
+import  Offcanvas  from "react-bootstrap/Offcanvas";
 
 
 
@@ -15,22 +15,36 @@ export default function NavBar(){
 
 
     return(
-        <Navbar bg='dark' variant="dark">
-        <Container>
+        <>
+       
+            <Navbar bg='dark' variant="dark" expand='md' className='mb-3'>
+        <Container fluid>
         <Navbar.Brand href='/'>Gregory Johnston</Navbar.Brand>
-        <Nav className="me-auto">
+        <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-lg`}/>
+        <Navbar.Offcanvas
+        className="bg-dark text-light"
+        id={`offcanvasNavarbar-expand-md`}
+        aria-labelledby={`offcanvasNavbar-expand-md`}
+        placement="end"
+        >
+        <Offcanvas.Header closeButton>
+            <Offcanvas.Title id='offcanvasNavbar-expand-md'>
+                Gregory Johnston
+            </Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+        <Nav className="justify-content-start flex-grow-1 pe-3">
             <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="/about">About</Nav.Link>
             <Nav.Link href='/projects'>Projects</Nav.Link>
             <Nav.Link href='/resume'>Resume</Nav.Link>
         </Nav>
+        </Offcanvas.Body>
+        </Navbar.Offcanvas>
         </Container>
-
-
-
-
-
-
         </Navbar>
+      
+     </>   
     )
+    
 }
