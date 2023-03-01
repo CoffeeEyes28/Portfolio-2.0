0c604486b89568c9c69388c9a21f3db4dfb101bd
Greg <litm47@gmail.com>
2023-03-01 14:27:27 -0500

    projects page is now fully responsive and sits in a card like system

--- src/data.js
+++ src/data.js
@@ -11,21 +11,27 @@ export const projects = [
         id: 1,
         title: 'Medley',
         description: 'web based interactive online music collection that allows users to showcase their taste to others in a quick and engaging manner',
+        tools: ' React, LastFM API, Material UI, Bootstrap, Node, Mongoose, NoSQL/MongoDB, Apollo, HTML, CSS',
         image: `${Medley}`,
-        link: 'https://medley.herokuapp.com/'
+        link: 'https://medley.herokuapp.com/',
+        repo: 'https://github.com/CoffeeEyes28/Medley'
     },
     {
         id: 2,
         title: 'Furrever',
-        description: 'web based social media platform designed to help get fostered animals adopted and provide information that will help keep them adopted.',
+        description: 'web based social media platform designed to help get fostered animals adopted and provide information that will help keep them adopted',
+        tools: 'HTML, Bulma CSS , JavaScript, Node, Express, Handlebars, Cloudinary, MySQL, Sequelize',
         image: `${Furrever}`,
-        link: 'https://enigmatic-gorge-61389.herokuapp.com/'
+        link: 'https://enigmatic-gorge-61389.herokuapp.com/',
+        repo: 'https://github.com/CoffeeEyes28/Furrever'
     },
     {
         id: 3,
         title: 'Anonymous Profile Generator',
-        description: 'web based application which will randomly generate a profile picture and bio asset for users to help quickly build an anonymous social media profile.',
+        description: 'web based application which will randomly generate a profile picture and bio asset for users to help quickly build an anonymous social media profile',
+        tools: 'HTML, CSS, Bootstrap, JavaScript, Pixel Encounter API, Random Fact Generator API',
         image: `${Anon}`,
-        link: 'https://dmerk2.github.io/Anon-Profile-Generator/'
+        link: 'https://dmerk2.github.io/Anon-Profile-Generator/',
+        repo: 'https://github.com/CoffeeEyes28/Anon-Profile-Generator'
     }
 ]
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -9,7 +9,6 @@ import  Carousel  from "react-bootstrap/Carousel";
 
 export default function Projects(){
 
-const [index, setIndex] = useState(0);
 
 
 
@@ -22,33 +21,38 @@ const [index, setIndex] = useState(0);
         exit={{y: 20, opacity: 0}}
         className="projects"
         >
-            <div className="d-flex align-items-center justify-content-center">
+            <div className="d-flex flex-row align-items-center justify-content-center">
                 <h1 className="text-white">Projects</h1>
             </div>
       
 <br></br>
 
 
-<div className="">
-
+<div className="container-fluid">
+<div className="row justify-content-center">
 {projects.map((project,id) => (
-<div className="d-flex flex-column align-items-center justify-content-center">
-    <div>
+<div className="col-lg-3 mx-md-2 mb-md-2 mt-md-2 bg-dark ">
+    <div className="d-flex justify-content-center py-2">
     <a href={project.link} target="_blank" rel="noreferrer">
     <img className="projectImg" src={project.image} alt={project.title}/>
     </a>
     </div>
     <br></br>
     <br></br>
-    <div className=" bg-dark w-50 text-center text-light">
+    <div className="bg-dark text-center text-light">
         <h2>{project.title}</h2>
         <p>{project.description}</p>
+        <br></br>
+        <h4>Built with:</h4>
+        <p>{project.tools}</p>
+        <a href={project.repo} target="_blank" rel="noreferrer"><p>GitHub</p></a>
+        <a href={project.link} target="_blank" rel="noreferrer">🔗</a>
 </div>
 <br></br>
-<br></br>
-    </div>
 
+    </div>
 ))}
+</div>
 </div>
 
 
